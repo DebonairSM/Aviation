40c1b6aaff549b235436d8a7d99e81306284aaf6
debonairsm <rommelb@gmail.com>
2025-03-14 18:40:45 -0300

    Refactor API documentation setup by replacing Swashbuckle with NSwag in Aircraft, Customers, and Subscriptions services. Updated Program.cs to configure OpenAPI document generation and Swagger UI integration.

--- AzureMicroservicesPlatform.Services.Subscriptions/Program.cs
+++ AzureMicroservicesPlatform.Services.Subscriptions/Program.cs
@@ -1,17 +1,22 @@
+using NSwag.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApiDocument(config => {
+    config.Title = "Subscriptions Service API";
+    config.Version = "v1";
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseOpenApi();
+    app.UseSwaggerUi();
 }
 
 app.UseHttpsRedirection();
